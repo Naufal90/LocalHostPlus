032bebfb460f4a9be06af6fc0ebb330bba8cb67e
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-28 23:18:41 +0700

    Update LocalHostPlusClientMod.java

--- src/client/java/naufal90/localhostplus/LocalHostPlusClientMod.java
+++ src/client/java/naufal90/localhostplus/LocalHostPlusClientMod.java
@@ -8,8 +8,6 @@ import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger("LocalHostPlus");
-
 public class LocalHostPlusClientMod implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("localhostplus-client");
     
