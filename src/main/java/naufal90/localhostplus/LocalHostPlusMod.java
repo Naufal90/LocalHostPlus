d0675e6ae1126176cf6b1cad66483e8657a8adff
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-28 13:58:47 +0700

    Update LocalHostPlusMod.java

--- src/main/java/naufal90/localhostplus/LocalHostPlusMod.java
+++ src/main/java/naufal90/localhostplus/LocalHostPlusMod.java
@@ -1,11 +1,6 @@
 package naufal90.localhostplus;
 
-import naufal90.localhostplus.network.ClientDiscovery;
-import naufal90.localhostplus.network.HotspotServer;
-import naufal90.localhostplus.screen.HotspotScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,15 +11,9 @@ public class LocalHostPlusMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         LOGGER.info("[LocalHostPlus] Mod Initialized!");
-        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-            if (screen instanceof TitleScreen) {
-                client.execute(() -> {
-                    client.setScreen(new HotspotScreen(screen));
-                });
-            }
-        });
 
-        // Start discovery listener
-        ClientDiscovery.startListening();
+        // Register your screen & networking
+        screen.HotspotScreen.register();
+        network.ClientDiscovery.init();
     }
 }
