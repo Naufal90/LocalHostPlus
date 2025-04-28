19b673af2c9b7f45190694ec84228223a69352d8
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-28 13:05:40 +0700

    Update ModConfig.java

---
+++ src/main/java/naufal90/localhostplus/config/ModConfig.java
@@ -0,0 +1,7 @@
+package naufal90.localhostplus.config;
+
+public class ModConfig {
+    public static boolean autoBroadcast = true;
+    public static int serverPort = 25565;
+    public static String serverMotd = "LocalHostPlus Server!";
+}
