2123305476d3ffe9f17c8990e42fdb8237e2c2da
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-28 22:50:12 +0700

    Create LocalHostPlusClientMod.java

---
+++ src/client/java/naufal90/localhostplus/LocalHostPlusClientMod.java
@@ -0,0 +1,15 @@
+// src/client/java/naufal90/localhostplus/LocalHostPlusClientMod.java
+
+package naufal90.localhostplus;
+
+import naufal90.localhostplus.screen.HotspotScreen;
+import naufal90.localhostplus.network.ClientDiscovery;
+import net.fabricmc.api.ClientModInitializer;
+
+public class LocalHostPlusClientMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        HotspotScreen.register();
+        ClientDiscovery.init();
+    }
+}
