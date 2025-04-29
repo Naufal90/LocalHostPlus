137e4f432c7e62863accc7f1d43b9c2414e0ae3a
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 05:53:31 +0700

    Update CustomGameMenuScreen.java

--- src/client/java/naufal90/localhostplus/screen/CustomGameMenuScreen.java
+++ src/client/java/naufal90/localhostplus/screen/CustomGameMenuScreen.java
@@ -2,7 +2,7 @@ import net.minecraft.text.Text;
 
 public class CustomGameMenuScreen extends GameMenuScreen {
     public CustomGameMenuScreen() {
-        super(Text.of("Custom Game Menu")); // Contoh jika membutuhkan Text
+        super(true); // Contoh jika membutuhkan Text
     }
 
     public ButtonWidget addCustomDrawableChild(ButtonWidget button) {
