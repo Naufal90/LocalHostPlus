a96e2b16d6539c23f6f7fc6ad369acdb81a625cb
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 06:39:12 +0700

    Update HotspotSettingsScreen.java

--- src/client/java/naufal90/localhostplus/screen/HotspotSettingsScreen.java
+++ src/client/java/naufal90/localhostplus/screen/HotspotSettingsScreen.java
@@ -1,16 +1,16 @@
 package naufal90.localhostplus.screen;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.gui.widget.ButtonWidget.PressAction;
 import net.minecraft.text.Text;
 import net.minecraft.client.gui.DrawContext;
 
+@Environment(EnvType.CLIENT)
 public class HotspotSettingsScreen extends Screen {
     private final Screen parent;
-    private ButtonWidget startButton;
 
     public HotspotSettingsScreen(Screen parent) {
         super(Text.literal("Hotspot Settings"));
@@ -19,51 +19,40 @@ public class HotspotSettingsScreen extends Screen {
 
     @Override
     protected void init() {
-        // Tombol Start Hotspot
-        startButton = this.addDrawableChild(
+        this.addDrawableChild(
             ButtonWidget.builder(
                 Text.literal("Start Open to Hotspot"),
-                button -> {
-                    // Mulai Hotspot disini
-                    startHotspot();
-                }
+                button -> startHotspot()
             ).position(this.width / 2 - 75, this.height / 2 - 10)
             .size(150, 20).build()
         );
 
-        // Tombol Kembali
         this.addDrawableChild(
             ButtonWidget.builder(
                 Text.literal("Back"),
-                button -> {
-                    this.client.setScreen(parent);
-                }
+                button -> this.client.setScreen(parent)
             ).position(this.width / 2 - 75, this.height / 2 + 20)
             .size(150, 20).build()
         );
     }
 
     private void startHotspot() {
-    if (this.client.getServer() instanceof IntegratedServer) {
-        IntegratedServer server = (IntegratedServer) this.client.getServer();
-        // null = pakai gamemode yang lagi dipakai, false = offline mode
-        server.openToLan(null, false, 25565);
+        if (this.client.getServer() instanceof IntegratedServer server) {
+            server.openToLan(null, false, 25565);
+            if (this.client.player != null) {
+                this.client.player.sendMessage(
+                    Text.literal("[Hotspot] Server berhasil dimulai di jaringan lokal!"),
+                    false
+                );
+            }
+        }
+        this.client.setScreen(null); // Tutup kembali ke game
     }
-
-    if (this.client.player != null) {
-        this.client.player.sendMessage(
-            Text.literal("[Hotspot] Server berhasil dimulai di jaringan lokal!"),
-            false
-        );
-    }
-    // Tutup screen kembali ke game
-    this.client.setScreen(null);
-}
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         this.renderBackground(context);
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+        context.drawCenteredTextWithShadow(context.getMatrices(), this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
         super.render(context, mouseX, mouseY, delta);
     }
 }
