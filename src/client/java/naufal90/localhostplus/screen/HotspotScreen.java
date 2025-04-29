36d6384468ee63ed513fa788a434806f2ad00207
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 05:43:31 +0700

    Update HotspotScreen.java

--- src/client/java/naufal90/localhostplus/screen/HotspotScreen.java
+++ src/client/java/naufal90/localhostplus/screen/HotspotScreen.java
@@ -1,27 +1,27 @@
 package naufal90.localhostplus.screen;
 
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import naufal90.localhostplus.screen.CustomGameMenuScreen;
 import naufal90.localhostplus.screen.HotspotSettingsScreen;
 
 public class HotspotScreen {
     public static void register() {
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-    if (screen instanceof GameMenuScreen) {
-    GameMenuScreen gameMenuScreen = (GameMenuScreen) screen;
-        ButtonWidget button = ButtonWidget.builder(
-            Text.literal("Hotspot Local Server"),
-            b -> client.setScreen(new HotspotSettingsScreen(screen))
-        )
-        .position(10, 10)
-        .size(150, 20)
-        .build();
+            if (screen instanceof CustomGameMenuScreen) {
+                CustomGameMenuScreen customScreen = (CustomGameMenuScreen) screen;
+                ButtonWidget button = ButtonWidget.builder(
+                    Text.literal("Hotspot Local Server"),
+                    b -> client.setScreen(new HotspotSettingsScreen(screen))
+                )
+                .position(10, 10)
+                .size(150, 20)
+                .build();
 
-        gameMenuScreen.addDrawableChild(button);
-    }
-});
+                customScreen.addCustomDrawableChild(button);
+            }
+        });
     }
 }
