ec5143dda9c44092a77362bfdd86ff8cbeb73346
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 06:31:36 +0700

    Create GameMenuScreenMixin.java

---
+++ src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
@@ -0,0 +1,30 @@
+package naufal90.localhostplus.mixin;
+
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import naufal90.localhostplus.screen.HotspotSettingsScreen;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(GameMenuScreen.class)
+public class GameMenuScreenMixin {
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo info) {
+        GameMenuScreen screen = (GameMenuScreen) (Object) this;
+
+        screen.addDrawableChild(
+            ButtonWidget.builder(
+                Text.literal("Open to Hotspot"),
+                button -> {
+                    MinecraftClient.getInstance().setScreen(new HotspotSettingsScreen(screen));
+                }
+            ).position(10, 10).size(150, 20).build()
+        );
+    }
+}
