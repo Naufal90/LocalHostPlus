b70a2093a0798b59756a068672e6cc4767403637
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 09:19:26 +0700

    Update GameMenuScreenMixin.java

--- src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
+++ src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
@@ -2,36 +2,37 @@ package naufal90.localhostplus.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import naufal90.localhostplus.screen.HotspotSettingsScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
-
     protected GameMenuScreenMixin(Text title) {
         super(title);
     }
 
-    @Shadow
-    protected abstract <T extends Element & Drawable & Selectable> T addDrawableChild(T element);
+    @Unique
+    private <T extends Element & Drawable & Selectable> void add(T element) {
+        this.addDrawableChild(element);
+    }
 
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo info) {
         ButtonWidget button = ButtonWidget.builder(
             Text.literal("Open to Hotspot"),
-            b -> MinecraftClient.getInstance().setScreen(new HotspotSettingsScreen((Screen)(Object)this))
+            b -> MinecraftClient.getInstance().setScreen(new HotspotSettingsScreen(this))
         ).position(10, 10).size(150, 20).build();
 
-        this.addDrawableChild(button);
+        add(button);
     }
 }
