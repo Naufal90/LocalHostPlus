da8be136b5503500ac7e50a5483feca151aad687
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-04-30 20:12:22 +0700

    Update Dummy.java

--- src/main/java/naufal90/localhostplus/Dummy.java
+++ src/main/java/naufal90/localhostplus/Dummy.java
@@ -4,5 +4,8 @@ public class Dummy {
     public static void forceInit() {
         // Referensikan class dari client agar masuk ke remap
         System.out.println("Dummy init: Linking to ClientModInitializer...");
+        System.out.println(HotspotScreen.class);
+        System.out.println(HotspotSettingsScreen.class);
+        System.out.println(ClientDiscovery.class);
     }
 }
