0b78500fd834deb6f7e1700a3327e42063f95e2e
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-05 22:43:54 +0700

    Update ScreenAccessor.java

--- src/client/java/naufal90/localhostplus/mixin/ScreenAccessor.java
+++ src/client/java/naufal90/localhostplus/mixin/ScreenAccessor.java
@@ -1,6 +1,8 @@
 package naufal90.localhostplus.mixin;
 
 import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -11,4 +13,19 @@ import java.util.List;
 public interface ScreenAccessor {
     @Accessor("drawables")
     List<Drawable> getDrawables();
+    
+    @Accessor("children")
+    List<Element> getChildren();
+    
+    @Accessor("selectables")
+    List<Selectable> getSelectables();
+    
+    @Accessor("drawables")
+    void setDrawables(List<Drawable> drawables);
+    
+    @Accessor("children")
+    void setChildren(List<Element> children);
+    
+    @Accessor("selectables")
+    void setSelectables(List<Selectable> selectables);
 }
