9feaeb7ed159a9bc3f122bd9c283b8455795992b
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-05 17:15:56 +0700

    Update GameMenuScreenMixin.java

--- src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
+++ src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
@@ -9,11 +9,14 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import naufal90.localhostplus.screen.HotspotSettingsScreen;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.List;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
