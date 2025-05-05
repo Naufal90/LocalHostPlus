deb21b9be6f9ceebcc8959927880697e6c0dedd8
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-05 21:21:44 +0700

    Create ScreenAccessor.java

---
+++ src/client/java/naufal90/localhostplus/mixin/ScreenAccessor.java
@@ -0,0 +1,14 @@
+package naufal90.localhostplus.mixin;
+
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(Screen.class)
+public interface ScreenAccessor {
+    @Accessor("drawables")
+    List<Drawable> getDrawables();
+}
