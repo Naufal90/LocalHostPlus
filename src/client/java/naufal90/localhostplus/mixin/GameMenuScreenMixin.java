3d787b726f6ea432875edec8f7e99fb9065a722d
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-05 16:05:16 +0700

    Update GameMenuScreenMixin.java

--- src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
+++ src/client/java/naufal90/localhostplus/mixin/GameMenuScreenMixin.java
@@ -21,20 +21,32 @@ public abstract class GameMenuScreenMixin extends Screen {
         super(title);
     }
 
-    @Unique
-    private <T extends Element & Drawable & Selectable> void add(T element) {
-        this.addDrawableChild(element);
+    //@Unique
+    //private <T extends Element & Drawable & Selectable> void add(T element) {
+        //this.addDrawableChild(element);
+    //}
+
+    @Inject(method = "initWidgets", at = @At("TAIL"))
+private void onInit(CallbackInfo info) {
+    int centerX = this.width / 2;
+    int buttonWidth = 204;
+    int buttonHeight = 20;
+
+    // Posisi target untuk tombol LocalHostPlus (di antara "Report Bugs" dan "Options...")
+    int insertY = this.height / 4 + 96;
+
+    // Geser tombol yang berada di bawah posisi target ke bawah
+    for (Drawable drawable : this.drawables) {
+        if (drawable instanceof ButtonWidget button && button.getY() >= insertY) {
+            button.setY(button.getY() + buttonHeight + 24); // geser ke bawah
+        }
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void onInit(CallbackInfo info) {
-        int centerX = this.width / 2;
-        int y = this.height / 4 + 72; // Di antara "Report Bugs" dan "Options..."
-        ButtonWidget button = ButtonWidget.builder(
-            Text.literal("LocalHostPlus"),
-            b -> MinecraftClient.getInstance().setScreen(new HotspotSettingsScreen(this))
-        ).position(centerX - 102, y).size(204, 20).build();
+    // Tambahkan tombol LocalHostPlus
+    ButtonWidget localhostButton = ButtonWidget.builder(
+        Text.literal("LocalHostPlus"),
+        b -> MinecraftClient.getInstance().setScreen(new HotspotSettingsScreen(this))
+    ).position(centerX - buttonWidth / 2, insertY).size(buttonWidth, buttonHeight).build();
 
-        add(button);
-    }
+    this.addDrawableChild(localhostButton);
 }
