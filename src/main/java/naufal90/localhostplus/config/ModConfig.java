94dc5437dfc597fc1457374707005851f1064d37
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-06 22:51:16 +0700

    Update ModConfig.java

--- src/main/java/naufal90/localhostplus/config/ModConfig.java
+++ src/main/java/naufal90/localhostplus/config/ModConfig.java
@@ -4,4 +4,12 @@ public class ModConfig {
     public static boolean autoBroadcast = true;
     public static int serverPort = 25565;
     public static String serverMotd = "LocalHostPlus Server!";
+    public static boolean pvp = true;
+    public static boolean allowCommands = true;
+    public static int maxPlayers = 8;
+    public static String gameMode = "survival"; // "creative", "adventure", etc.
+
+    public static void saveConfig() {
+        // TODO: optional, for saving to file
+    }
 }
