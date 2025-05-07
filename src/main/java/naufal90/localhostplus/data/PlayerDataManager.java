d71598e39862d0a1a9ccf97bd550a08f9b8025f6
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-07 07:16:28 +0700

    Update PlayerDataManager.java

--- src/main/java/naufal90/localhostplus/data/PlayerDataManager.java
+++ src/main/java/naufal90/localhostplus/data/PlayerDataManager.java
@@ -20,7 +20,7 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Base64
+import java.util.Base64;
 
 public class PlayerDataManager {
     private static final String DATA_DIR = "config/localhostplus/playerdata/";
