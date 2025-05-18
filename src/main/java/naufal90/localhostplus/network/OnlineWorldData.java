867d3fb8e59509cbc58a126af0caf82c3488b68e
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-19 06:50:47 +0700

    Create OnlineWorldData.java

---
+++ src/main/java/naufal90/localhostplus/network/OnlineWorldData.java
@@ -0,0 +1,10 @@
+package naufal90.localhostplus.network;
+
+public class OnlineWorldData {
+    public String uuid;
+    public String ip;
+    public int port;
+    public String worldName;
+    public String motd;
+    public int maxPlayers;
+}
