cbb27c710b58094f8438399a3965e5d3ee58aad8
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-19 13:42:35 +0700

    Update OnlinePlayScreen.java

--- src/client/java/naufal90/localhostplus/screen/OnlinePlayScreen.java
+++ src/client/java/naufal90/localhostplus/screen/OnlinePlayScreen.java
@@ -1,18 +1,22 @@
 package naufal90.localhostplus.screen;
 
-import naufal90.localhostplus.network.OnlineHostFetcher;
-import naufal90.localhostplus.network.OnlineWorldData;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.ElementListWidget;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.widget.EntryListWidget;
 import net.minecraft.text.Text;
+import naufal90.localhostplus.network.OnlineHostFetcher;
+import naufal90.localhostplus.network.OnlineWorldData;
 
+import java.util.Collections;
 import java.util.List;
 
 public class OnlinePlayScreen extends Screen {
-    private WorldListWidget worldList;
+    private OnlineWorldListWidget worldList;
     private List<OnlineWorldData> worlds;
 
     public OnlinePlayScreen() {
@@ -21,38 +25,33 @@ public class OnlinePlayScreen extends Screen {
 
     @Override
     protected void init() {
-        super.init();
-
         this.worlds = OnlineHostFetcher.fetchHosts();
-        this.worldList = new WorldListWidget(this.client, this.width, this.height, 40, this.height - 40, 24);
 
+        this.worldList = new OnlineWorldListWidget(this.client, this.width, this.height, 40, this.height - 60, 24);
         for (OnlineWorldData world : worlds) {
-            this.worldList.addEntry(new WorldEntry(world));
+            this.worldList.addEntry(new OnlineWorldListWidget.WorldEntry(world));
         }
+        this.addSelectableChild(this.worldList);
 
-        this.addSelectableChild(worldList);
-
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Refresh"), btn -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Refresh"), button -> {
             this.client.setScreen(new OnlinePlayScreen());
-        }).position(this.width / 2 - 100, this.height - 28)
-          .size(200, 20).build());
+        }).position(this.width / 2 - 100, this.height - 50).size(200, 20).build());
 
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Back"), btn -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Back"), button -> {
             this.client.setScreen(new HotspotSettingsScreen(null));
-        }).position(this.width / 2 - 100, this.height - 52)
-          .size(200, 20).build());
+        }).position(this.width / 2 - 100, this.height - 25).size(200, 20).build());
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 12, 0xFFFFFF);
-        worldList.render(matrices, mouseX, mouseY, delta);
-        super.render(matrices, mouseX, mouseY, delta);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context);
+        context.drawCenteredText(this.textRenderer, this.title, this.width / 2, 12, 0xFFFFFF);
+        this.worldList.render(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
     }
 
-    private static class WorldListWidget extends ElementListWidget<WorldEntry> {
-        public WorldListWidget(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight) {
+    public static class OnlineWorldListWidget extends EntryListWidget<OnlineWorldListWidget.WorldEntry> {
+        public OnlineWorldListWidget(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight) {
             super(client, width, height, top, bottom, itemHeight);
         }
 
@@ -65,28 +64,38 @@ public class OnlinePlayScreen extends Screen {
         public int getRowWidth() {
             return this.width - 12;
         }
-    }
 
-    private static class WorldEntry extends ElementListWidget.Entry<WorldEntry> {
-        private final OnlineWorldData data;
+        public static class WorldEntry extends EntryListWidget.Entry<WorldEntry> {
+            private final OnlineWorldData data;
 
-        public WorldEntry(OnlineWorldData data) {
-            this.data = data;
-        }
+            public WorldEntry(OnlineWorldData data) {
+                this.data = data;
+            }
 
-        @Override
-        public void render(MatrixStack matrices, int index, int y, int x, int entryWidth, int entryHeight,
-                           int mouseX, int mouseY, boolean hovered, float tickDelta) {
-            MinecraftClient client = MinecraftClient.getInstance();
-            client.textRenderer.draw(matrices, "World: " + data.worldName, x + 4, y + 2, 0xFFFFFF);
-            client.textRenderer.draw(matrices, "Host: " + data.ip + ":" + data.port, x + 4, y + 12, 0xAAAAAA);
-        }
+            @Override
+            public void render(DrawContext context, int index, int y, int x, int entryWidth, int entryHeight,
+                               int mouseX, int mouseY, boolean hovered, float delta) {
+                TextRenderer tr = MinecraftClient.getInstance().textRenderer;
+                context.drawText(tr, "World: " + data.worldName, x + 4, y + 2, 0xFFFFFF, false);
+                context.drawText(tr, "Host: " + data.ip + ":" + data.port, x + 4, y + 12, 0xAAAAAA, false);
+            }
 
-        @Override
-        public boolean mouseClicked(double mouseX, double mouseY, int button) {
-            System.out.println("[LocalHostPlus] Connecting to " + data.ip + ":" + data.port);
-            // Tambahkan metode join server-mu sendiri di sini
-            return true;
+            @Override
+            public List<? extends Element> children() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public List<? extends Selectable> selectableChildren() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public boolean mouseClicked(double mouseX, double mouseY, int button) {
+                System.out.println("[LocalHostPlus] Connecting to " + data.ip + ":" + data.port);
+                // TODO: Tambahkan koneksi ke server
+                return true;
+            }
         }
     }
 }
