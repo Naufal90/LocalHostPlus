3b58c2cf104258bf7c710a9b13cb8218f4fb1c71
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-19 23:53:35 +0700

    Update HotspotServer.java

--- src/client/java/naufal90/localhostplus/network/HotspotServer.java
+++ src/client/java/naufal90/localhostplus/network/HotspotServer.java
@@ -4,6 +4,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.server.integrated.IntegratedServer;
+import naufal90.localhostplus.utils.PlayerUUIDManager;
 import naufal90.localhostplus.LocalHostPlusMod;
 
 public class HotspotServer {
@@ -13,19 +14,24 @@ public class HotspotServer {
     if (client.getServer() instanceof IntegratedServer) {
         IntegratedServer server = (IntegratedServer) client.getServer();
         try {
-            server.openToLan(server.getSaveProperties().getGameMode(), server.getSaveProperties().isHardcore(), 25565);
+            server.openToLan(
+                server.getSaveProperties().getGameMode(),
+                server.getSaveProperties().isHardcore(),
+                25565
+            );
             LocalHostPlusMod.LOGGER.info("[Hotspot] Server published on LAN!");
 
-            String username = client.getSession().getUsername(); // Tambahkan ini
-            Broadcaster.startBroadcast(username, server.getServerPort()); // Gunakan username
+            String username = client.getSession().getUsername();
+            String uuid = PlayerUUIDManager.getOfflineUUID(username).toString(); // Generate UUID
+            Broadcaster.startBroadcast(uuid, server.getServerPort()); // Gunakan UUID
+
         } catch (Exception e) {
             LocalHostPlusMod.LOGGER.error("[Hotspot] Failed to open LAN: ", e);
         }
     }
-    }
+}
     
-    public static void start(String username, MinecraftServer server) {
-        Broadcaster.startBroadcast(username, server.getServerPort());
+    public static void start(String uuid, MinecraftServer server) {
+    Broadcaster.startBroadcast(uuid, server.getServerPort());
     }
-    
 }
