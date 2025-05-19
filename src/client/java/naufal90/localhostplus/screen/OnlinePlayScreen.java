0171841a84175f46a0a91eb1ef29c7d5a41943f1
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-05-19 12:39:37 +0700

    Update OnlinePlayScreen.java

--- src/client/java/naufal90/localhostplus/screen/OnlinePlayScreen.java
+++ src/client/java/naufal90/localhostplus/screen/OnlinePlayScreen.java
@@ -1,47 +1,43 @@
 package naufal90.localhostplus.screen;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import naufal90.localhostplus.network.OnlineHostFetcher;
 import naufal90.localhostplus.network.OnlineWorldData;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.EntryListWidget;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.gui.widget.ElementListWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
 import java.util.List;
 
 public class OnlinePlayScreen extends Screen {
-
-    private EntryListWidget worldListWidget;
+    private WorldListWidget worldList;
     private List<OnlineWorldData> worlds;
 
-    protected OnlinePlayScreen() {
-        super(Text.of("Online Worlds"));
+    public OnlinePlayScreen() {
+        super(Text.literal("Online Worlds"));
     }
 
     @Override
     protected void init() {
         super.init();
 
-        worlds = OnlineHostFetcher.fetchHosts();
-        worldListWidget = new EntryListWidget(this.client, this.width, this.height, 32, this.height - 32, 24);
+        this.worlds = OnlineHostFetcher.fetchHosts();
+        this.worldList = new WorldListWidget(this.client, this.width, this.height, 40, this.height - 40, 24);
 
         for (OnlineWorldData world : worlds) {
-            worldListWidget.addEntry(new WorldEntry(world));
+            this.worldList.addEntry(new WorldEntry(world));
         }
 
-        this.addSelectableChild(worldListWidget);
+        this.addSelectableChild(worldList);
 
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Refresh"), button -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Refresh"), btn -> {
             this.client.setScreen(new OnlinePlayScreen());
         }).position(this.width / 2 - 100, this.height - 28)
           .size(200, 20).build());
 
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Back"), button -> {
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Back"), btn -> {
             this.client.setScreen(new HotspotSettingsScreen(null));
         }).position(this.width / 2 - 100, this.height - 52)
           .size(200, 20).build());
@@ -51,11 +47,27 @@ public class OnlinePlayScreen extends Screen {
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
         drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 12, 0xFFFFFF);
-        worldListWidget.render(matrices, mouseX, mouseY, delta);
+        worldList.render(matrices, mouseX, mouseY, delta);
         super.render(matrices, mouseX, mouseY, delta);
     }
 
-    class WorldEntry extends EntryListWidget.Entry<WorldEntry> {
+    private static class WorldListWidget extends ElementListWidget<WorldEntry> {
+        public WorldListWidget(MinecraftClient client, int width, int height, int top, int bottom, int itemHeight) {
+            super(client, width, height, top, bottom, itemHeight);
+        }
+
+        @Override
+        protected int getScrollbarPositionX() {
+            return this.width - 6;
+        }
+
+        @Override
+        public int getRowWidth() {
+            return this.width - 12;
+        }
+    }
+
+    private static class WorldEntry extends ElementListWidget.Entry<WorldEntry> {
         private final OnlineWorldData data;
 
         public WorldEntry(OnlineWorldData data) {
@@ -68,18 +80,12 @@ public class OnlinePlayScreen extends Screen {
             MinecraftClient client = MinecraftClient.getInstance();
             client.textRenderer.draw(matrices, "World: " + data.worldName, x + 4, y + 2, 0xFFFFFF);
             client.textRenderer.draw(matrices, "Host: " + data.ip + ":" + data.port, x + 4, y + 12, 0xAAAAAA);
-
-            // Add connect button
-            client.textRenderer.draw(matrices, "[Join]", x + 200, y + 6, 0x00FF00);
         }
 
         @Override
         public boolean mouseClicked(double mouseX, double mouseY, int button) {
-            // Simple check if "Join" area was clicked
-            if (mouseX > 200 && mouseX < 250) {
-                MinecraftClient.getInstance().openScreen(null);
-                MinecraftClient.getInstance().joinMultiplayerServer(data.ip, data.port);
-            }
+            System.out.println("[LocalHostPlus] Connecting to " + data.ip + ":" + data.port);
+            // Tambahkan metode join server-mu sendiri di sini
             return true;
         }
     }
